068bf1c7024cb2c325f0000a95112c8518f935d0
PedroLuis <pedroluis0542@gmail.com>
2021-06-08 23:43:31 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import gql from 'graphql-tag'
+import { Mutation, ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+export const UPLOAD_FILE = gql`
+   mutation uploadFile($file: Upload!) {
+     uploadFile(file: $file) {
+     filename
+   }
+}`;
+//create apollo client
+const client = new ApolloClient({
+                 link: ApolloLink.from([
+                       onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message, locations, path }) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
+if (networkError) console.log(`[Network error]: ${networkError}`);}),
+new createUploadLink({ uri: 'http://localhost:4000/graphql'})
+]),
+cache: new InMemoryCache()
+});
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <div className="App">
+      <ApolloProvider client={client}>
+         <Mutation mutation={UPLOAD_FILE}>
+           {uploadFile => (<input type="file" required onChange= {({target: { validity, files: [file] } }) =>validity.valid && uploadFile({ variables: { file } })}/>)}
+         </Mutation>
+      </ApolloProvider>
+  </div>);
 }
-
 export default App;
